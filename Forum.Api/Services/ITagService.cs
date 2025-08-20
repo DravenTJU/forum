c7c1c8d7c4dba1c2eba9e4cf4df2c9fa0c3671d5
timmy <102719366+tiiiiiimmy@users.noreply.github.com>
2025-08-21 11:59:37 +1200

    ✨ feat: 添加标签服务接口
    - 创建 ITagService 标签服务接口
    - 定义标签管理方法契约

---
+++ Forum.Api/Services/ITagService.cs
@@ -0,0 +1,10 @@
+using Forum.Api.Models.Entities;
+
+namespace Forum.Api.Services;
+
+public interface ITagService
+{
+    Task<IEnumerable<Tag>> GetAllAsync();
+    Task<Tag?> GetByIdAsync(long id);
+    Task<Tag?> GetBySlugAsync(string slug);
+}
